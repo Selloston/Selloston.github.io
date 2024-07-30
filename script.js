8c5c35e993e305005022f1a78df97356769da2fc
Selloston <aymankader539@gmail.com>
2024-07-30 12:20:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let divLogoText = document.getElementById("hoverLogo");
+let hoverServies = document.getElementById("hoverServies");
+let hoverProjects = document.getElementById("hoverProjects");
+let hoverHelp = document.getElementById("hoverHelp");
+
+function colorLogo()
+{
+    divLogoText.style.backgroundColor = "#5a189a";
+}
+
+function removeColorLogo()
+{
+    divLogoText.style.backgroundColor = "";
+}
+
+function colorServies()
+{
+    hoverServies.style.backgroundColor = "#5a189a";
+}
+
+function removeColorServies()
+{
+    hoverServies.style.backgroundColor = "";
+}
+
+function colorProjects()
+{
+    hoverProjects.style.backgroundColor = "#5a189a";
+}
+
+function removeColorProjects()
+{
+    hoverProjects.style.backgroundColor = "";
+}
+
+function colorHelp()
+{
+    hoverHelp.style.backgroundColor = "#5a189a";
+}
+
+function removeColorHelp()
+{
+    hoverHelp.style.backgroundColor = "";
+}
